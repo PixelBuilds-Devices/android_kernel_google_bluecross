zachariasmaladroit/kernel_lge_msm8998

mmc: queue: Do not attempt request merging
According to Admin Magazine, request merging is mostly for rotation
disks in order to reduce seek times by grouping together requests. This
reduces the frequency of disk spin-ups. However, flash storage does not
suffer from long disk seeks. Disable request merging in order to reduce
overhead and improve throughput.

Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
(cherry picked from commit 821c94fb601855be8b79fcfa305b34a1d421d993)
Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>

drivers/mmc/card/queue.c
@@ -378,6 +378,7 @@ int mmc_init_queue(struct mmc_queue *mq, struct mmc_card *card,
 
 	blk_queue_prep_rq(mq->queue, mmc_prep_request);
 	queue_flag_set_unlocked(QUEUE_FLAG_NONROT, mq->queue);
+	queue_flag_set_unlocked(QUEUE_FLAG_NOMERGES, mq->queue);
 	queue_flag_clear_unlocked(QUEUE_FLAG_ADD_RANDOM, mq->queue);
 	if (mmc_can_erase(card))
 		mmc_queue_setup_discard(mq->queue, card);

PixelBuilds-Devices/android_kernel_google_bluecross

Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

drivers/mmc/card/queue.c
@@ -397,6 +397,7 @@ int mmc_init_queue(struct mmc_queue *mq, struct mmc_card *card,
 
 	blk_queue_prep_rq(mq->queue, mmc_prep_request);
 	queue_flag_set_unlocked(QUEUE_FLAG_NONROT, mq->queue);
+	queue_flag_set_unlocked(QUEUE_FLAG_NOXMERGES, mq->queue);
 	queue_flag_clear_unlocked(QUEUE_FLAG_ADD_RANDOM, mq->queue);
 	if (mmc_can_erase(card))
 		mmc_queue_setup_discard(mq->queue, card);